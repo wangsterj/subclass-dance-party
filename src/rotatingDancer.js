36c956b268cac2c529d25945cdb7c47f5f41891b
wangsterj <wang.j@berkeley.edu>
2019-07-23 16:22:13 -0700

    Added rotate method

--- src/rotatingDancer.js
+++ src/rotatingDancer.js
@@ -1,6 +1,7 @@
-var RotatingDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<div class="RotatingDancer"></div>');
+var RotatingDancer = function(top, left, timeBetweenSteps, id) {
+  Dancer.call(this, top, left, Math.random(50,100)*100);
+  this.$node = $('<div class="rotatingDancer"><img src="images/spiral.jpg" class="spiral"></div>');
+  this.rotation = 0;
   this.setPosition(top, left);
 };
 
@@ -15,8 +16,18 @@ RotatingDancer.prototype.oldStep = Dancer.prototype.step;
 RotatingDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   this.oldStep();
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  // this.$node.toggle();
+
+  this.rotate(this.rotation);
+  if (!this.nearNeighbor) {
+    this.rotation = this.rotation + 10;
+  } else {
+    this.rotation = this.rotation - 10;
+  }
+};
+
+RotatingDancer.prototype.rotate = function(rotation) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  this.styleSettings.transform = `rotate(${rotation}deg)`;
+  this.$node.css(this.styleSettings);
 };
